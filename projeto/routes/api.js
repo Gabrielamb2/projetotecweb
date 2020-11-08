9e34c174229007471842940c60bd93bc25a73e20
Gabriela Boriero <gabrielamb2@al.inper.edu.br>
2020-11-08 11:39:44 -0300

    .

--- projeto/routes/api.js
+++ projeto/routes/api.js
@@ -1,20 +1,25 @@
 var unirest = require("unirest");
-
+var express = require('express');
+var router = express.Router();
 var req = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer");
+// req.query({
+// 	"q": "How much vitamin c is in 2 apples?"
+// });
+// req.headers({
+// 	"x-rapidapi-key": "a189d94715msh94688240afe7d21p18a442jsn5712d79e456d",
+// 	"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+// 	"useQueryString": true
+// });
+// req.end(function (res) {
+// 	if (res.error) throw new Error(res.error);
 
-req.query({
-	"q": "How much vitamin c is in 2 apples?"
+// 	console.log(res.body);
+// });
+router.get('/oi', function(req, res, next) {
+	res.body
+	
+	
 });
 
-req.headers({
-	"x-rapidapi-key": "a189d94715msh94688240afe7d21p18a442jsn5712d79e456d",
-	"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-	"useQueryString": true
-});
 
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
+module.exports = router;
--- projeto/routes/index.js
+++ projeto/routes/index.js
@@ -1,24 +1,49 @@
 var express = require('express');
 var router = express.Router();
-
+var unirest = require("unirest");
+var reqi = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 /* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-  var db = require("../db");
-  var Users = db.Mongoose.model('usercollection', db.UserSchema,
-'usercollection');
-  Users.find({}).lean().exec(
-     function (e, docs) {
-      res.json(docs);
-      res.end();
+// router.get('/userlist', function(req, res) {
+//   var db = require("../db");
+//   var Users = db.Mongoose.model('usercollection', db.UserSchema,
+// 'usercollection');
+//   Users.find({}).lean().exec(
+//      function (e, docs) {
+//       res.json(docs);
+//       res.end();
+//   });
+// });
+
+/* GET New User page. */
+router.get('/', function(req, res) {
+  res.render('pesquisa', { title: 'Pesquisar' });
   });
+
+  /* POST to Add User Service */
+router.post('/pesquisar', function (req, res) {
+  var pesquisa = req.body.pesquisa;
+  console.log(pesquisa);
+  reqi.query({
+	"q": pesquisa
 });
-/* GET Userlist page. */
-router.get('/api', function(req, res) {
-  res.render('index', { title: 'API' });
+reqi.headers({
+	"x-rapidapi-key": "a189d94715msh94688240afe7d21p18a442jsn5712d79e456d",
+	"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+	"useQueryString": true
+});
+reqi.end(function (res) {
+	if (res.error) throw new Error(res.error);
+  var resposta = res.body.answer
+  console.log(res.body.answer);
+  // res.render('resultado', { resposta: resposta });
+});
+
+  
   });
+
 module.exports = router;
