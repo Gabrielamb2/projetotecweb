4ad6badfaa0d1c6924810935ba15bf83af7c3c8e
Gabriela Boriero <gabrielamb2@al.inper.edu.br>
2020-11-08 20:59:37 -0300

    .

--- projeto/routes/index.js
+++ projeto/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var unirest = require("unirest");
-var reqi = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer");
+
 
 
 /* GET Resposta rapida CALCULAR. */
@@ -10,58 +10,96 @@ router.get('/', function(req, res) {
   });
 
   /* POST  Resposta rapida CALCULAR*/
-router.post('/buscar', function (req, res) {
-  var pesquisa = req.body.pesquisa;
-  console.log(pesquisa);
-  reqi.query({
-	"q": pesquisa
-});
-reqi.headers({
-	"x-rapidapi-key": "a189d94715msh94688240afe7d21p18a442jsn5712d79e456d",
-	"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-	"useQueryString": true
-});
-reqi.end(function (res) {
-	if (res.error) throw new Error(res.error);
-// RESPOSTA 
-  console.log(res.body.answer);
- 
-});
-
-
+// router.post('/buscar', function (req, res) {
+//   var pesquisa = req.body.pesquisa;
+//   console.log(pesquisa);
+  
+// });
+router.get('/buscar/:word',(req, res)=> {
+  const requ = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer");
+  requ.query({
+    "q": req.params.word
   });
+  requ.headers({
+    "x-rapidapi-key": "a189d94715msh94688240afe7d21p18a442jsn5712d79e456d",
+    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "useQueryString": true
+  });
+  requ.end(function (response) {
+    if (response.error) throw new Error(response.error);
+    res.json(response.body);
+    
+  });
+    
+    // res.json(res.body);
+    // res.end();
+  
+      // console.log(req);
+      // var parsedBody = JSON.parse(res)
+      // res.send(parsedBody)
+    
+  // RESPOSTA 
+    
+   
+  });
+
+
+  
+
+
 
   
     /* POST  CONVERTER*/
-  router.post('/converter', function (req, res) {
-    var ingrediente  = req.body.ingre;
-    var numero  = req.body.valor;
-    var med_antes  = req.body.medicao_antes;
-    var med_dps  = req.body.medicao_depois;
-    console.log(ingrediente)
-    console.log(numero)
-    console.log(med_antes)
-    console.log(med_dps)
-    var req = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/convert");
+//   router.post('/converter', function (req, res) {
+//     var ingrediente  = req.body.ingre;
+//     var numero  = req.body.valor;
+//     var med_antes  = req.body.medicao_antes;
+//     var med_dps  = req.body.medicao_depois;
+//     console.log(ingrediente)
+//     console.log(numero)
+//     console.log(med_antes)
+//     console.log(med_dps)
+//     var req = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/convert");
 
-    req.query({
-      "ingredientName": ingrediente,
-      "targetUnit": med_dps,
-      "sourceUnit": med_antes,
-      "sourceAmount": numero
+//     req.query({
+//       "ingredientName": ingrediente,
+//       "targetUnit": med_dps,
+//       "sourceUnit": med_antes,
+//       "sourceAmount": numero
+//     });
+
+//     req.headers({
+//       "x-rapidapi-key": "a189d94715msh94688240afe7d21p18a442jsn5712d79e456d",
+//       "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+//       "useQueryString": true
+//     });
+
+
+//     req.end(function (res) {
+//       if (res.error) throw new Error(res.error);
+// // RESPOSTA 
+//       console.log(res.body);
+//     });
+//   });
+  router.get('/converter/:ingrediente/:med_dps/:med_antes/:numero',(req, res)=> {
+    const requ = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/convert");
+    requ.query({
+      "ingredientName": req.params.ingrediente,
+    "targetUnit": req.params.med_dps,
+    "sourceUnit": req.params.med_antes,
+    "sourceAmount":req.params.numero
+      
     });
-
-    req.headers({
+    requ.headers({
       "x-rapidapi-key": "a189d94715msh94688240afe7d21p18a442jsn5712d79e456d",
       "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
       "useQueryString": true
     });
 
-
-    req.end(function (res) {
-      if (res.error) throw new Error(res.error);
-// RESPOSTA 
-      console.log(res.body);
+    requ.end(function (response) {
+      if (response.error) throw new Error(response.error);
+      res.json(response.body);
+      
     });
   });
 
@@ -139,6 +177,8 @@ router.post('/substituto/ingredientes', function(req, res) {
   });
   });
 
+
+// curiosidades  
 router.post('/curiosidades', function(req, res) {
   var unirest = require("unirest");
 
@@ -157,4 +197,30 @@ router.post('/curiosidades', function(req, res) {
     console.log(res.body);
   });
   });
+
+// Criar cartão de receita
+router.post('/criareceita', function(req, res) {
+  res.render('criar_cartao_receita');
+  });
+
+router.post('/receitanova', function(req, res) {
+  var unirest = require("unirest");
+
+  var req = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/trivia/random");
+
+  req.headers({
+    "x-rapidapi-key": "a189d94715msh94688240afe7d21p18a442jsn5712d79e456d",
+    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "useQueryString": true
+  });
+
+
+  req.end(function (res) {
+    if (res.error) throw new Error(res.error);
+
+    console.log(res.body);
+  });
+  });
+
+
 module.exports = router;
