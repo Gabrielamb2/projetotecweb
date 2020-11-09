eadc40716b8c32b5a5fa8562b7b4cbab999877f3
Gabriela Boriero <gabrielamb2@al.inper.edu.br>
2020-11-09 12:06:23 -0300

    .

--- projeto/routes/index.js
+++ projeto/routes/index.js
@@ -90,24 +90,7 @@ router.get('/substituto/:ingrediente', function(req, res) {
 
 
 // curiosidades  
-router.get('/curiosidades', function(req, res) {
-  var unirest = require("unirest");
 
-  var requ = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/trivia/random");
-
-  requ.headers({
-    "x-rapidapi-key": "a189d94715msh94688240afe7d21p18a442jsn5712d79e456d",
-    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-    "useQueryString": true
-  });
-
-
-  requ.end(function (response) {
-    if (response.error) throw new Error(response.error);
-    res.json(response.body);
-    
-  });
-  });
 
   // Combine receitas com calorias diárias 
 router.get('/receitascaloria/:numerocal/:prazo', function(req, res) {
